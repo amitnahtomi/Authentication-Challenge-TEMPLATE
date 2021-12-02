a669c2395db7097adb387f0935cf1a245b6df497
amitnahtomi <amit.nahtomi@gmail.com>
2021-12-02 02:27:01 +0200

    12 tsets passed

--- app.js
+++ app.js
@@ -2,12 +2,11 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const { json } = require('express');
 const saltRounds = bcrypt.genSaltSync(10);
 const app = express();
 app.use(express.json());
 
-const USERS = [] 
+const USERS = [{ email: "admin@email.com", name: "admin", password: bcrypt.hashSync('Rc123456!', saltRounds), isAdmin: true }] 
 const INFORMATION = []
 const REFRESHTOKENS = []
 
@@ -19,7 +18,7 @@ app.post('/users/register', (req, res, next)=>{
             res.end();
         }
     }
-    if(req.body.password === 'Rc123456!'){
+    if(req.body.password !== 'Rc123456!'){
          admin = false;
     }
     else {
@@ -47,8 +46,8 @@ app.post('/users/login', (req, res ,next)=>{
        res.end();
     }
     else {
-        const token = jwt.sign(target, 'Rc123456!', {expiresIn: '10h'});
-        const rToken = (REFRESHTOKENS.length).toString();
+        const token = jwt.sign(target, 'Rc123456!', {expiresIn: '10s'});
+        const rToken = jwt.sign(target, 'Rc123456!', {expiresIn: '10h'}) 
         REFRESHTOKENS.push(rToken);
         res.status(200).json({accessToken: token, refreshToken: rToken , email: target.email, name: target.name, isAdmin: target.isAdmin})
         res.end();
@@ -98,18 +97,23 @@ app.get("/api/v1/information", (req, res, next)=>{
 app.post("/users/token", (req, res, next)=>{
     const token = req.body.token;
     if(token === undefined) {
-        res.status(401).send("Refresh Token Required")
+        res.status(401).send("Refresh Token Required");
+        res.end();
     }
     if(!REFRESHTOKENS.includes(token)) {
         res.status(403).send("Invalid Refresh Token");
         res.end();
+        return;
     }
     else {
-        const user = jwt.verify(token, 'Rc123456!')
-        const newToken = jwt.sign(user, 'Rc123456!', {expiresIn: '10s'})
-        REFRESHTOKENS.splice(REFRESHTOKENS.indexOf(token), 1);
-        REFRESHTOKENS.push(newToken);
-        res.status(200).send(newToken)
+        const user = jwt.verify(token, 'Rc123456!');
+        try{
+        const newToken = jwt.sign(user, 'Rc123456!', {});
+        res.status(200).json({accessToken: newToken})
+        }
+        catch(err) {
+            console.log(err);
+        }
     }
 })
 
@@ -129,60 +133,65 @@ app.post("/users/logout", (req, res, next)=>{
 })
 
 app.get('/api/v1/users', (req, res, next)=>{
-    const token = req.headers.Authorization.split(" ")[1];
+    const token = req.header('Authorization').split(" ")[1];
     if(token === undefined) {
         res.status(401).send("Access Token Required")
     }
-    if(!REFRESHTOKENS.includes(token)) {
-        res.status(403).send("Invalid Access Token");
-        res.end();
-    }
     else {
-        const user = jwt.verify(token, 'Rc123456!')
+    try{
+        jwt.verify(token, 'Rc123456!');
+        }
+        catch {
+            res.status(403).send("Invalid Access Token");
+        res.end();
+        }
+        const user = jwt.verify(token, 'Rc123456!');
         if(user.isAdmin === false) {
             res.end();
             return;
         }
         else {
-            res.status(200).send({USERS: USERS})
+            res.status(200).send(USERS)
         }
     }
 })
 
 app.options('/', (req, res, next)=>{
-    const token = req.headers.Authorization.split(" ")[1];
-    if(token === undefined) {
+    if(req.header('Authorization') === undefined) {
         res.status(200).setHeader('allow', '"OPTIONS, GET, POST"').send([{ method: "post", path: "/users/register", description: "Register, Required: email, name, password", example: { body: { email: "user@email.com", name: "user", password: "password" } } },
         { method: "post", path: "/users/login", description: "Login, Required: valid email and password", example: { body: { email: "user@email.com", password: "password" } } }])
         res.end();
     }
-    else if(!REFRESHTOKENS.includes(token)) {
-        res.status(200).setHeader('allow', '"OPTIONS, GET, POST"').send([{ method: "post", path: "/users/register", description: "Register, Required: email, name, password", example: { body: { email: "user@email.com", name: "user", password: "password" } } },
+    const token = req.header('Authorization').split(" ")[1];
+    try {
+        jwt.verify(token, 'Rc123456!');
+    }
+    catch {
+        res.status(200).setHeader('allow', 'OPTIONS, GET, POST').send([{ method: "post", path: "/users/register", description: "Register, Required: email, name, password", example: { body: { email: "user@email.com", name: "user", password: "password" } } },
         { method: "post", path: "/users/login", description: "Login, Required: valid email and password", example: { body: { email: "user@email.com", password: "password" } } },
         { method: "post", path: "/users/token", description: "Renew access token, Required: valid refresh token", example: { headers: { token: "\*Refresh Token\*" } } }])
     }
+    const user = jwt.verify(token, 'Rc123456!');
+    if(user.isAdmin === false) {
+        res.status(200).setHeader('allow', '"OPTIONS, GET, POST"').send([{ method: "post", path: "/users/register", description: "Register, Required: email, name, password", example: { body: { email: "user@email.com", name: "user", password: "password" } } },
+        { method: "post", path: "/users/login", description: "Login, Required: valid email and password", example: { body: { email: "user@email.com", password: "password" } } },
+        { method: "post", path: "/users/token", description: "Renew access token, Required: valid refresh token", example: { headers: { token: "\*Refresh Token\*" } } },
+        { method: "post", path: "/users/tokenValidate", description: "Access Token Validation, Required: valid access token", example: { headers: { Authorization: "Bearer \*Access Token\*" } } },
+        { method: "get", path: "/api/v1/information", description: "Access user's information, Required: valid access token", example: { headers: { Authorization: "Bearer \*Access Token\*" } } },
+        { method: "post", path: "/users/logout", description: "Logout, Required: access token", example: { body: { token: "\*Refresh Token\*" } } }])
+    }
     else {
-        const user = jwt.verify(token, 'Rc123456');
-        if(user.isAdmin === false) {
-            res.status(200).setHeader('allow', '"OPTIONS, GET, POST"').send([{ method: "post", path: "/users/register", description: "Register, Required: email, name, password", example: { body: { email: "user@email.com", name: "user", password: "password" } } },
+        res.status(200).setHeader('allow', '"OPTIONS, GET, POST"').send([
+            { method: "post", path: "/users/register", description: "Register, Required: email, name, password", example: { body: { email: "user@email.com", name: "user", password: "password" } } },
             { method: "post", path: "/users/login", description: "Login, Required: valid email and password", example: { body: { email: "user@email.com", password: "password" } } },
             { method: "post", path: "/users/token", description: "Renew access token, Required: valid refresh token", example: { headers: { token: "\*Refresh Token\*" } } },
             { method: "post", path: "/users/tokenValidate", description: "Access Token Validation, Required: valid access token", example: { headers: { Authorization: "Bearer \*Access Token\*" } } },
             { method: "get", path: "/api/v1/information", description: "Access user's information, Required: valid access token", example: { headers: { Authorization: "Bearer \*Access Token\*" } } },
-            { method: "post", path: "/users/logout", description: "Logout, Required: access token", example: { body: { token: "\*Refresh Token\*" } } }])
-        }
-        else {
-            res.status(200).setHeader('allow', '"OPTIONS, GET, POST"').send([
-                { method: "post", path: "/users/register", description: "Register, Required: email, name, password", example: { body: { email: "user@email.com", name: "user", password: "password" } } },
-                { method: "post", path: "/users/login", description: "Login, Required: valid email and password", example: { body: { email: "user@email.com", password: "password" } } },
-                { method: "post", path: "/users/token", description: "Renew access token, Required: valid refresh token", example: { headers: { token: "\*Refresh Token\*" } } },
-                { method: "post", path: "/users/tokenValidate", description: "Access Token Validation, Required: valid access token", example: { headers: { Authorization: "Bearer \*Access Token\*" } } },
-                { method: "get", path: "/api/v1/information", description: "Access user's information, Required: valid access token", example: { headers: { Authorization: "Bearer \*Access Token\*" } } },
-                { method: "post", path: "/users/logout", description: "Logout, Required: access token", example: { body: { token: "\*Refresh Token\*" } } },
-                { method: "get", path: "api/v1/users", description: "Get users DB, Required: Valid access token of admin user", example: { headers: { authorization: "Bearer \*Access Token\*" } } }
-              ])
-        }
-    }
+            { method: "post", path: "/users/logout", description: "Logout, Required: access token", example: { body: { token: "\*Refresh Token\*" } } },
+            { method: "get", path: "api/v1/users", description: "Get users DB, Required: Valid access token of admin user", example: { headers: { authorization: "Bearer \*Access Token\*" } } }
+          ])
+    }   
+    
 })
 
 module.exports = app;
